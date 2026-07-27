722d6933032d480dcb08185d35a202a699870126
Khazz <thelenaid@gmail.com>
2026-07-27 11:27:55 +0700

    api.js

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+
 const komikController = require('../controller/komikController');
+const userController = require("../controller/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.get('/komik', komikController.getAllKomik);
-router.get('/komik/:id', komikController.getKomikById);
-router.post('/komik', komikController.createKomik);
-router.put('/komik/:id', komikController.updateKomik);
-router.delete('/komik/:id', komikController.deleteKomik);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
 
+//public
+router.get("/Komik", komikController.getAllKomik);
+router.get("/Komik/:id", komikController.getKomikById);
+
+//protected
+router.post("/Komik", authMiddleware, komikController.createKomik);
+router.put("/Komik/:id", authMiddleware, komikController.updateKomik);
+router.delete("/Komik/:id", authMiddleware, komikController.deleteKomik);
 module.exports = router;
