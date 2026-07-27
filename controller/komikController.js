cf8df1919216c4dfe3321940a77a8852eda50a21
Khazz <thelenaid@gmail.com>
2026-07-27 09:44:26 +0700

    async function getKomikById

--- controller/komikController.js
+++ controller/komikController.js
@@ -10,3 +10,18 @@ async function getAllKomik(req, res) {
     }
 }
 
+async function getKomikById(req, res) {
+
+    const { id } = req.params;
+    try{
+        const Komik = await db.Komik.findByPk(id);
+        if (!komik){
+            return res.status(404).json({ error: 'Komik not found'});
+        }
+        res.status(200).json( Komik)
+    }catch (err) {
+        console.error('Error fetching komik by ID:', err.message);
+        res.status(500).json({ error: 'Failed to fetch komik by ID'});
+    }
+}
+
