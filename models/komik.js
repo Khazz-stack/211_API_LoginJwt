297a389c1785d4ebb9c30004bc4bc75b7265e1a6
Khazz <thelenaid@gmail.com>
2026-07-27 09:50:07 +0700

    menambahkan autor

--- models/komik.js
+++ models/komik.js
@@ -13,7 +13,9 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
         },
 
-       
+        author: {
+            type: DataTypes.STRING,
+        },
     });
 
     return Komik;
