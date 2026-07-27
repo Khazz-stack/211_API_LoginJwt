2907892d04de9affac6d170c3b5f89b895e86921
Khazz <thelenaid@gmail.com>
2026-07-27 09:49:16 +0700

    menambahkan title

--- models/komik.js
+++ models/komik.js
@@ -5,6 +5,10 @@ module.exports = (sequelize, DataTypes) => {
             autoIncrement: true,
             primaryKey: true
         },
+        title: {
+            type: DataTypes.STRING,
+        },
+
         
     });
 
